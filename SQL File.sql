8a4dbe33dcdae90cf27d6f98133c9c2ecb73c348
Yasmine Elsadat <yasmine.atef2004@gmail.com>
2023-11-15 00:54:25 +0200

    added views

--- SQL File.sql
+++ SQL File.sql
@@ -36,7 +36,7 @@ CONSTRAINT FK_Student FOREIGN KEY(advisor_id) references Advisor(advisor_id)
 );
 
 CREATE TABLE Course(
-course_id int PRIMARY KEY IDENTITY,
+course_id int PRIMARY KEY,
 name varchar(40) NOT NULL,
 major varchar(40) NOT NULL,
 is_offered bit NOT NULL,
@@ -133,8 +133,8 @@ CONSTRAINT FK_SIC3 FOREIGN KEY(instructor_id) references Instructor(instructor_i
 );
 
 CREATE TABLE MakeUp_Exam(
-exam_id int PRIMARY KEY,
-date date NOT NULL,
+exam_id int PRIMARY KEY IDENTITY,
+date datetime NOT NULL,
 type varchar(40) NOT NULL check(type IN ('First_makeup','Second_makeup')),
 course_id int not null,
 CONSTRAINT FK_MU FOREIGN KEY (course_id) references Course(course_id)
@@ -194,7 +194,7 @@ EXEC CreateAllTables;
 ----------------------------------- TEST VALUES ---------------------------------------
 INSERT INTO Advisor VALUES('ahmed','hi@gmail.com','C5','cheese');
 INSERT INTO Student VALUES('ali','z',2.1,'eng','@','MET','batetes',1,4,3,2,1);
-INSERT INTO Course VALUES('math','eng',1,23,1);
+INSERT INTO Course VALUES(43,'math','eng',1,23,1);
 INSERT INTO Request VALUES(5435,'a','hi','pending',13,1,1,1);
 INSERT INTO Semester VALUES('1','1-11-2000','1-12-2000');
 INSERT INTO Payment VALUES(4364783,46967239,'1-11-2000',1,'notPaid',50,1,'1','11-2-2000');
@@ -276,3 +276,75 @@ DELETE FROM Student;
 DELETE FROM Advisor;
 GO
 --EXEC clearAllTables
+
+--******************  Basic Data Retrieval  *****************
+
+CREATE VIEW view_Students AS
+SELECT * FROM Student;
+-- SELECT * FROM view_Students;
+
+GO
+CREATE VIEW view_Course_prerequisites AS
+SELECT C.*,PC.prerequisite_course_id 
+FROM Course C LEFT OUTER JOIN 
+     PreqCourse_course PC ON C.course_id=PC.course_id;
+--- SELECT * FROM view_Course_prerequisites;
+
+GO 
+CREATE VIEW Instructors_AssignedCourses AS
+SELECT I.*,C.name AS 'course_name'     --? do we include ID too?
+FROM Instructor I INNER JOIN 
+     Instructor_Course IC ON I.instructor_id=IC.instructor_id INNER JOIN
+     Course C ON IC.course_id=C.course_id;
+-- SELECT * FROM Instructors_AssignedCourses;
+
+GO
+CREATE VIEW Student_Payment AS
+SELECT S.f_name,S.l_name ,P.*  --? get student's full name plus ID?
+FROM Payment P INNER JOIN 
+     Student S ON P.student_id=S.student_id;
+-- SELECT * FROM Student_Payment;
+
+GO 
+CREATE VIEW Courses_Slots_Instructor AS
+SELECT C.course_id,C.name AS 'course_name',S.slot_id, S.day,S.time,S.location,I.name AS 'instructor_name'
+FROM Course C LEFT OUTER JOIN 
+     Instructor_Course IC ON C.course_id=IC.course_id LEFT OUTER JOIN
+     Slot S ON S.course_id=C.course_id AND S.instructor_id=IC.instructor_id LEFT OUTER JOIN
+     Instructor I ON I.instructor_id=IC.instructor_id;
+-- SELECT * FROM Courses_Slots_Instructor;
+
+GO
+CREATE VIEW Courses_MakeupExams AS
+SELECT C.name,C.semester, M.exam_id,M.date,M.type
+FROM Course C LEFT OUTER JOIN 
+     MakeUp_Exam M ON C.course_id=M.course_id;
+-- SELECT * FROM Courses_MakeupExams;     
+
+GO 
+CREATE VIEW Students_Courses_transcript AS  --? semester student took in the code format not numeric?
+SELECT S.student_id,S.f_name,S.l_name,C.course_id,C.name AS 'course_name',T.exam_type,T.grade,T.semester_code,I.name AS 'instructor_name'
+FROM Student_Instructor_Course_Take T INNER JOIN
+     STUDENT S ON S.student_id=T.student_id INNER JOIN
+     Instructor I ON I.instructor_id=T.instructor_id INNER JOIN
+     Course C ON T.course_id=C.course_id;
+-- SELECT * FROM Students_Courses_transcript; 
+
+GO 
+CREATE VIEW Semester_offered_Courses AS
+SELECT C.course_id,C.name,S.semester_code
+FROM Semester S LEFT OUTER JOIN
+     Course_Semester CS ON S.semester_code=CS.semester_code LEFT OUTER JOIN
+     Course C ON C.course_id=CS.course_id;
+-- SELECT * FROM Semester_offered_Courses; 
+
+GO 
+CREATE VIEW Advisors_Graduation_Plan AS
+SELECT G.plan_id,G.semester_code,G.semester_credit_hours,G.expected_grad_semester,
+G.student_id,G.advisor_id,A.name as 'advisor_name'
+FROM Graduation_Plan G INNER JOIN
+     Advisor A ON G.advisor_id=A.advisor_id;
+-- SELECT * FROM Advisors_Graduation_Plan; 
+
+--******************  All Other Requirements  *****************
+      
